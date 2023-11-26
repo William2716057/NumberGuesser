f086bf6fcaa6e368d77f775e1af0cd35a6330b54
William Spriggs <Willspri@gmail.com>
2023-11-27 12:56:08 +1300

    Updated

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -13,10 +13,15 @@ namespace NumberGuesser
         //entry point method
         static void Main(string[] args)
         {
-            string name = "User";
-            int age = 20;
-            Console.WriteLine(name + " is " + age);
-            Console.WriteLine("{0 is {1}", name, age);
+            //string name = "User";
+            //int age = 20;
+            //Console.WriteLine(name + " is " + age);
+            //Console.WriteLine("{0 is {1}", name, age);
+            string appName = "Number Guessr";
+            string appVersion = "1.0.0";
+            string appAuthor = "Person";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
         }
     }
 }
