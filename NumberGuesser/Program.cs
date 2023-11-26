caa42ae06bf80087cc7902d502f8a98e6b1e1f77
William Spriggs <Willspri@gmail.com>
2023-11-27 12:46:32 +1300

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+namespace NumberGuesser
+{
+    //main class
+    internal class Program
+    {
+        //entry point method
+        static void Main(string[] args)
+        {
+        }
+    }
+}
